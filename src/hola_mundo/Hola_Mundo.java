2e593ba5bae7577dc4f86bf62074b1f46db1fe76
Marvin <aguirreestuardo758@gmail.com>
2022-07-20 21:50:23 -0600

    Se creo una calculadora, se implemento funcion suma

--- src/hola_mundo/Hola_Mundo.java
+++ src/hola_mundo/Hola_Mundo.java
@@ -1,12 +1,23 @@
 
 package hola_mundo;
 
+import java.util.Scanner;
+
 
 public class Hola_Mundo {
 
    
     public static void main(String[] args) {
-        System.out.println("HOLA MUNDO");
+        
+        Scanner sc =new Scanner (System.in);
+        System.out.println("Bienvenido, esta es mi calculadora\n"
+                + "Ingresa el primer numero a operar");
+        int a= sc.nextInt();
+        System.out.println("Ingresa el segundo numero a operar");
+        int b =sc.nextInt();
+        System.out.println("La suma de las dos cantidades es: " + (a+b));
+       
+        
     }
     
 }
